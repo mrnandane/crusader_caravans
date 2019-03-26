c525b25cd67265484f415852d43965d3546b51ed
mayur.nandane <mrnandane@gmail.com>
2019-03-26 22:55:18 +0530

    initial commit

---
+++ build/precache-manifest.1a2712d284ef2c714d34adc18ef9198b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/static/media/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "368143b14b70790255d8",
+    "url": "/static/css/main.d21173a1.chunk.css"
+  },
+  {
+    "revision": "3fc9f9f6c2c52a0e136612c6475135fe",
+    "url": "/static/media/fontello.3fc9f9f6.svg"
+  },
+  {
+    "revision": "d674bc351828edfe0d59",
+    "url": "/static/js/1.d674bc35.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2fffa85cde9949a9cd995bf9525dd2a4",
+    "url": "/static/media/crusader-logo.2fffa85c.svg"
+  },
+  {
+    "revision": "368143b14b70790255d8",
+    "url": "/static/js/main.368143b1.chunk.js"
+  },
+  {
+    "revision": "023c9cbc5484fa6d8aafc5d3723465cd",
+    "url": "/static/media/fontello.023c9cbc.woff2"
+  },
+  {
+    "revision": "381441fc6288547b64a0f7bf3450d9c0",
+    "url": "/static/media/fontello.381441fc.woff"
+  },
+  {
+    "revision": "f4ab96f8ea56352015c127d37c22a1a1",
+    "url": "/static/media/fontello.f4ab96f8.eot"
+  },
+  {
+    "revision": "d37a1d10b9e5ed44aef0fd6eacd24f7d",
+    "url": "/static/media/fontello.d37a1d10.ttf"
+  },
+  {
+    "revision": "d674bc351828edfe0d59",
+    "url": "/static/css/1.d631c9d3.chunk.css"
+  },
+  {
+    "revision": "5be99a2409228b70944aa8d4104e20af",
+    "url": "/index.html"
+  }
+];
